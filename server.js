b062fdd57db65a0c66825f46faa57287971d513d
adarsh301998 <30661476+adarsh301998@users.noreply.github.com>
2017-08-06 18:44:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/:articleName', function(req, res){
-  var artcleName = req.params.articleName;
+  var articleName = req.params.articleName;
   res.send(createTemplate(article[articleName]));  
 });
 
