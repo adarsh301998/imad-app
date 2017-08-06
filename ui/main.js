382c501d0707f38befcb3c56b51b32295e9ffeb9
adarsh301998 <30661476+adarsh301998@users.noreply.github.com>
2017-08-07 01:03:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,5 @@
 console.log('Loaded!');
-// MAking chages in html though java script
 
-var element = document.getElementById("main-text");
-element.innerHTML="Adarsh";
-
-// Moving image
-
-var marginLeft=0;
-function moveRight(){
- marginLeft = marginLeft + 10;
- m.style.marginLeft = marginLeft +'px';
-}
-
-var m = document.getElementById("img");
-m.onclick = function() {
-  var interval = setInterval(moveRight, 100); 
-};
 var counter = 0; 
 var button = document.getElementById("counter");
 button.onclick = function() {
