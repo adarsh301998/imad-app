6637586648a70bea82090003cebd69e1dfdc7bc4
adarsh301998 <30661476+adarsh301998@users.noreply.github.com>
2017-08-06 10:56:09 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one', function(req, res){
+  res.send("This is article one");  
+});
+app.get('/article-two', function(req, res){
+  res.send("This is article TWO");  
+});
+app.get('/article-three', function(req, res){
+  res.send("This is article THREE");  
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
